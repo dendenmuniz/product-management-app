1cbd853cad214bfa83a82db56ed1337811202964
dendenmuniz <dendenmuniz@gmail.com>
2025-03-06 11:09:37 +0100

    chore: setup express server with basic middleware and routes

---
+++ server/src/server.ts
@@ -0,0 +1,25 @@
+import express from "express";
+import cors from "cors";
+import dotenv from "dotenv";
+import helmet from "helmet";
+
+import authRoutes from "./routes/auth";
+import productsRoutes from "./routes/products";
+
+dotenv.config();
+
+const app = express();
+
+app.use(cors());
+app.use(helmet());
+app.use(express.json());
+
+app.use("/auth", authRoutes);
+app.use("/products", productsRoutes);
+
+app.get("/", (req, res) => {
+    res.send("API is running!");
+});
+
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
