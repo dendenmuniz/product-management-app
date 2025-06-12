e58d9d3565932dfecfe821c959e0a7652215fc0d
dendenmuniz <dendenmuniz@gmail.com>
2025-06-12 15:46:42 +0200

    feat: add ItemAttributeArray component to support array fields

---
+++ client/src/components/ItemAttributeArray.tsx
@@ -0,0 +1,81 @@
+import { useState } from "react";
+
+interface Props {
+  attribute: React.ReactNode;
+  values: string[];
+  name: string;
+  isEditing: boolean;
+  onChange: (name: string, newValues: string[]) => void;
+}
+
+export const ItemAttributeArray = ({ attribute, values, name, isEditing, onChange }: Props) => {
+  const [inputValue, setInputValue] = useState("");
+
+  const handleAdd = () => {
+    const trimmed = inputValue.trim();
+    if (trimmed && !values.includes(trimmed)) {
+      onChange(name, [...values, trimmed]);
+      setInputValue("");
+    }
+  };
+
+  const handleRemove = (index: number) => {
+    const updated = [...values];
+    updated.splice(index, 1);
+    onChange(name, updated);
+  };
+
+  return (
+   <div className="border-b border-base-300 w-full ">
+  <div className="flex flex-col gap-2 w-full">
+    {isEditing ? (
+      <>
+        <div className="flex items-start justify-between gap-2 w-full">
+          <fieldset className="fieldset w-full">
+            <legend className="fieldset-legend ">{attribute}</legend>
+            <input
+              type="text"
+              className="input input-sm input-bordered w-full"
+              name={name}
+              value={inputValue}
+              onChange={(e) => setInputValue(e.target.value)}
+            />
+            <div className="flex flex-wrap gap-2 mt-2">
+              {values.map((val, i) => (
+                <div key={i} className="badge badge-outline gap-1">
+                  {val}
+                  <button type="button" className="ml-1" onClick={() => handleRemove(i)}>✕</button>
+                </div>
+              ))}
+            </div>
+          </fieldset>
+
+          <button
+            type="button"
+            className="btn btn-sm btn-outline self-start mt-[30px]" // aligns with input
+            onClick={handleAdd}
+          >
+            Add
+          </button>
+        </div>
+      </>
+    ) : values.length > 0 ? (
+       <fieldset className="fieldset w-full">
+            <legend className="fieldset-legend">{attribute}</legend>
+      <div className="flex flex-wrap justify-start gap-2">
+        {values.map((val, i) => (
+          <span key={i} className="badge badge-ghost text-sm">{val}</span>
+        ))}
+      </div>
+      </fieldset>
+    ) : (
+       <fieldset className="fieldset w-full">
+            <legend className="fieldset-legend">{attribute}</legend>
+      <span className="text-sm text-gray-300">N/A</span>
+      </fieldset>
+    )}
+  </div>
+</div>
+
+  );
+};
